ad54e4c001b386d2484bc9c9f9783d6e9e72347f
ZTRCOMPANY1 <ztrcompany1@gmail.com>
2025-07-18 23:06:05 -0300

    sadsasadas

---
+++ gtag.js
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HV53WHG9V3"></script>
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HV53WHG9V3');
